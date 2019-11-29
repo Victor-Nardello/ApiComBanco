2355291c1e2720a043752128060b4d11f29a1746
Victor Nardello <victornardello@olosko.com>
2019-11-29 09:14:18 -0300

    Adicionando metodo FindById no Controller

--- src/main/java/com/carrosapi/carros/controller/CarrosController.java
+++ src/main/java/com/carrosapi/carros/controller/CarrosController.java
@@ -1,8 +1,10 @@
 package com.carrosapi.carros.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,10 @@ public class CarrosController {
 	@RequestMapping(value = "/carros", method = RequestMethod.GET)
 	public List<CarrosDomain> showAllCarros() {
 		return carrosRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/carros/{id}", method = RequestMethod.GET)
+	public Optional<CarrosDomain> showAllCarrosById(@PathVariable Long id) {
+		return carrosRepository.findById(id);
 	}
 }
